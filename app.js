ac2e175ce3525cf2374b8336b72905768276db4c
rathinabalavilathan@gmail.com <rathinabalavilathan@gmail.com>
2022-11-01 19:26:57 -0400

    doc write

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const port = process.port || 3000;
 app.get('/',(req, res) => res.send('hello world'));
 app.listen(port,() => 
 {
-    console.log('REST API running on port 3000');
+    document.write('app running in ' + port);
+    console.log('REST API running on port ' + port);
 }
 )
